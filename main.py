35742c2f72ddd865faf53b68a4350056cfada754
shreiya25bce11110 <shreiya.25bce11110@vitbhopal.ac.in>
2026-03-26 23:47:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ from colorama import Fore, Style, init
 # to start or initialize colorama
 init(autoreset=True)
 
+#motivational qoutes to stay motivated
+quotes = [
+    "Stay focused, you're doing great!",
+    "Small progress is still progress.",
+    "Discipline beats motivation.",
+    "Push yourself, no one else will.",
+    "Success starts with consistency."
+]
 
+def show_quote():
+    print(Fore.CYAN + " \n ✨" + random.choice(quotes) + "\n")
 
 def start_session():
     task = input("Please input the name of the task: ")
@@ -16,22 +26,22 @@ def start_session():
     distractions = 0
 
     while True:
-        print("\nOptions:")
+        print(Fore.BLUE + "\nOptions:")
         print("1. Log distraction")
         print("2. End session")
         
-        choice = input("Enter choice: ")
+        choice = input("Enter your choice from the above options: ")
 
         if choice == "1":
             distractions += 1
-            print("Distraction noted! ⚠️ Stay focused!")
+            print(Fore.RED + "Distraction noted! ⚠️ Stay focused!")
 
         elif choice == "2":
             end_time = time.time()
             duration = end_time - start_time
             minutes = round(duration / 60, 2)
 
-            print("\n📊 Session Summary")
+            print(Fore.MAGENTA + "\n Session Summary")
             print(f"Task: {task}")
             print(f"Time Studied: {minutes} minutes")
             print(f"Distractions: {distractions}")
@@ -40,7 +50,17 @@ def start_session():
             break
 
         else:
-            print("Invalid choice!")
+            print(Fore.RED + "Invalid choice!")
+def pomodoro():
+    print(Fore.GREEN + "\n🍅 Pomodoro started: 25 minutes focus!")
+    show_quote()
+   
+    time.sleep(1500) # 25 minutes
+   
+    print(Fore.YELLOW + "\n⏸️ Time for a 5-minute break!")
+    time.sleep(300)
+
+    print(Fore.GREEN + "\n✅ Pomodoro session complete!")
             
 def save_session(task, minutes, distractions):
     date = datetime.now().strftime("%Y-%m-%d")
@@ -48,41 +68,51 @@ def save_session(task, minutes, distractions):
     with open("data.txt", "a") as file:
         file.write(f"{date},{task},{minutes},{distractions}\n")
 
-    print("Session saved successfully ✅")
+    print(Fore.GREEN + "Session saved successfully ✅")
+    
 def view_summary():
     try:
         with open("data.txt", "r") as file:
             lines = file.readlines()
 
             if not lines:
-                print("No data available.")
+                print(Fore.YELLOW + "No data available.")
                 return
 
-            print("\n📅 Study History:\n")
+            print(Fore.CYAN + "\n📅 Study History:\n")
+            total_time = 0
+            
             for line in lines:
                 date, task, minutes, distractions = line.strip().split(",")
+                total_time += float(minutes)
                 print(f"{date} | {task} | {minutes} mins | {distractions} distractions")
+            
+            print(Fore.GREEN + f"\n📈 Total Study Time: {round(total_time,2)} minutes")
 
     except FileNotFoundError:
         print("No study sessions found yet.")
+        
 def main():
     while True:
-        print("\n==== FocusFlow ====")
+        print(Fore.CYAN + "\n==== FocusFlow ====")
         print("1. Start Study Session")
-        print("2. View Summary")
-        print("3. Exit")
+        print("2. Pomodoro Mode")
+        print("3. View Summary")
+        print("4. Exit")
 
         choice = input("Enter your choice: ")
 
         if choice == "1":
             start_session()
         elif choice == "2":
-            view_summary()
+            pomodoro()
         elif choice == "3":
-            print("Goodbye! Stay productive 🚀")
+            view_summary()
+        elif choice == "4":
+            print(Fore.GREEN + "Goodbye! Stay productive.")
             break
         else:
-            print("Invalid choice!")
+            print(Fore.RED + "Invalid choice!")
 
 if __name__ == "__main__":
     main()
